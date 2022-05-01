96466112a3b4559cad303991a9d920e8715eea80
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-04-30 22:51:16 -0500

    Working on the inputs for the forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointments",
+      day: "May 27, 2022",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Picking up grocery foods",
+      day: "May 3rd",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Meeting with the school",
+      day: "May 10th",
+      reminder: false,
+    },
+  ]);
+
+  //Adding tasks
+  const addTask = (task) => {
+    console.log(task);
+  };
+
+  // Deleting function to erase the tasks
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggling the reminder for the tasks
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks at the moment"
+      )}
     </div>
   );
 }
